6a33c835ea5387f0ae90e0557e3d2641d92c235e
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-06-09 22:47:31 -0400

    Dispite using genymotion emulator, there is still a failure for the FusedLocationApi

--- app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
+++ app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
@@ -1,39 +1,93 @@
 package com.nullcognition.udacitygoogleplayservices;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.Location;
+import android.util.Log;
+import android.widget.TextView;
 
-// asynchronous programming model
-/*
-onCcreate - googleApiClient
-onStart - googleApiClient
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-onconnectionfailed, suspended, connected - location request, locationchanged - location update
-
-location service
-*/
+public class ActivityMain extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
 
-public class ActivityMain extends Activity{
+	private final String LOG_TAG ="LaurenceTestApp";
+	private TextView txtv;
+	private GoogleApiClient mGoogleApiClient;
+	private LocationRequest mLocationRequest;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addApi(LocationServices.API)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.build();
+
+		txtv = (TextView) findViewById(R.id.txtv);
+	}
+
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mGoogleApiClient.connect();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mGoogleApiClient.disconnect();
+		super.onStop();
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+
+		mLocationRequest = LocationRequest.create();
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		mLocationRequest.setInterval(10); // Update location every second
+
+		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+		Log.i(LOG_TAG, "GoogleApiClient connection has been suspend");
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		Log.i(LOG_TAG, "GoogleApiClient connection has failed");
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		Log.i(LOG_TAG, location.toString());
+
+		txtv.setText(Double.toString(location.getLatitude()));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_activity_main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
+	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if(id == R.id.action_settings){ return true;}
+		if (id == R.id.action_settings) {return true;}
 		return super.onOptionsItemSelected(item);
 	}
 }
