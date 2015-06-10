d29123bb88fff72f47ee49ce793194202e0732ac
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-06-10 13:03:54 -0400

    last known, and continuous updates

--- app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
+++ app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
@@ -10,18 +10,21 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class ActivityMain extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+public class ActivityMain extends Activity implements GoogleApiClient.ConnectionCallbacks,
+                                                      GoogleApiClient.OnConnectionFailedListener,
+                                                      com.google.android.gms.location.LocationListener{
 
 
 	private final String LOG_TAG = "LaurenceTestApp";
 	private TextView txtLat;
 	private TextView txtLon;
+	private TextView txtLastKnown;
 	private GoogleApiClient googleApiClient;
 	private LocationRequest locationRequest;
+	private Location lastLocation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -32,9 +35,9 @@ public class ActivityMain extends Activity implements GoogleApiClient.Connection
 
 		txtLat = (TextView) findViewById(R.id.txtLat);
 		txtLon = (TextView) findViewById(R.id.txtLon);
-
+		txtLastKnown = (TextView) findViewById(R.id.txtLastKnown);
 	}
-	private synchronized void buildGoogleApiClient(){
+	protected synchronized void buildGoogleApiClient(){
 		googleApiClient = new GoogleApiClient.Builder(this)
 				.addApi(LocationServices.API)
 				.addConnectionCallbacks(this)
@@ -53,30 +56,36 @@ public class ActivityMain extends Activity implements GoogleApiClient.Connection
 	@Override
 	protected void onStop(){
 		// Disconnecting the client invalidates it.
-		googleApiClient.disconnect();
 		super.onStop();
+		if(googleApiClient.isConnected()){googleApiClient.disconnect();}
 	}
 
 	@Override
 	public void onConnected(Bundle bundle){
 
+		continuousLocationUpdates();
+		lastKnownUpdate();
+	}
+	private void continuousLocationUpdates(){
 		locationRequest = LocationRequest.create();
 		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 		locationRequest.setInterval(10); // Update location every second
-
 		// location accuracy from really fine, to 100m to 10 km
-
 		LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this); // still crashing
+	}
 
-
-
+	private void lastKnownUpdate(){
+		lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+		if(lastLocation != null){
+			txtLastKnown.setText(String.valueOf(lastLocation.getLatitude() + " " + lastLocation.getLongitude()));
+		}
 	}
 
 	@Override
 	public void onConnectionSuspended(int i){Log.i(LOG_TAG, "GoogleApiClient connection has been suspend");}
 
 	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult){		Log.i(LOG_TAG, "GoogleApiClient connection has failed" + connectionResult.getErrorCode());}
+	public void onConnectionFailed(ConnectionResult connectionResult){ Log.i(LOG_TAG, "GoogleApiClient connection has failed" + connectionResult.getErrorCode());}
 
 	@Override
 	public void onLocationChanged(Location location){
