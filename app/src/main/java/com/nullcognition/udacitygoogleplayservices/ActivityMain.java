fe179f6cb074debbef6cf2b3009a01294a25a90b
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-06-10 12:29:53 -0400

    meta data tag placed in the application tag, still crashing at LocationServices.FusedLocationApi.req...

--- app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
+++ app/src/main/java/com/nullcognition/udacitygoogleplayservices/ActivityMain.java
@@ -1,10 +1,11 @@
 package com.nullcognition.udacitygoogleplayservices;
+
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.location.Location;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -13,81 +14,88 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class ActivityMain extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class ActivityMain extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
 
-	private final String LOG_TAG ="LaurenceTestApp";
-	private TextView txtv;
-	private GoogleApiClient mGoogleApiClient;
-	private LocationRequest mLocationRequest;
+	private final String LOG_TAG = "LaurenceTestApp";
+	private TextView txtLat;
+	private TextView txtLon;
+	private GoogleApiClient googleApiClient;
+	private LocationRequest locationRequest;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
+		buildGoogleApiClient();
+
+		txtLat = (TextView) findViewById(R.id.txtLat);
+		txtLon = (TextView) findViewById(R.id.txtLon);
+
+	}
+	private synchronized void buildGoogleApiClient(){
+		googleApiClient = new GoogleApiClient.Builder(this)
 				.addApi(LocationServices.API)
 				.addConnectionCallbacks(this)
 				.addOnConnectionFailedListener(this)
 				.build();
-
-		txtv = (TextView) findViewById(R.id.txtv);
 	}
 
 
 	@Override
-	protected void onStart() {
+	protected void onStart(){
 		super.onStart();
 		// Connect the client.
-		mGoogleApiClient.connect();
+		googleApiClient.connect();
 	}
 
 	@Override
-	protected void onStop() {
+	protected void onStop(){
 		// Disconnecting the client invalidates it.
-		mGoogleApiClient.disconnect();
+		googleApiClient.disconnect();
 		super.onStop();
 	}
 
 	@Override
-	public void onConnected(Bundle bundle) {
+	public void onConnected(Bundle bundle){
 
-		mLocationRequest = LocationRequest.create();
-		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-		mLocationRequest.setInterval(10); // Update location every second
+		locationRequest = LocationRequest.create();
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		locationRequest.setInterval(10); // Update location every second
 
-		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+		// location accuracy from really fine, to 100m to 10 km
+
+		LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this); // still crashing
+
+
 
 	}
 
 	@Override
-	public void onConnectionSuspended(int i) {
-		Log.i(LOG_TAG, "GoogleApiClient connection has been suspend");
-	}
+	public void onConnectionSuspended(int i){Log.i(LOG_TAG, "GoogleApiClient connection has been suspend");}
 
 	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		Log.i(LOG_TAG, "GoogleApiClient connection has failed");
-	}
+	public void onConnectionFailed(ConnectionResult connectionResult){		Log.i(LOG_TAG, "GoogleApiClient connection has failed" + connectionResult.getErrorCode());}
 
 	@Override
-	public void onLocationChanged(Location location) {
+	public void onLocationChanged(Location location){
 		Log.i(LOG_TAG, location.toString());
 
-		txtv.setText(Double.toString(location.getLatitude()));
+		txtLat.setText(Double.toString(location.getLatitude()));
+		txtLon.setText(Double.toString(location.getLongitude()));
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu){
 		getMenuInflater().inflate(R.menu.menu_activity_main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item){
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {return true;}
+		if(id == R.id.action_settings){return true;}
 		return super.onOptionsItemSelected(item);
 	}
 }
