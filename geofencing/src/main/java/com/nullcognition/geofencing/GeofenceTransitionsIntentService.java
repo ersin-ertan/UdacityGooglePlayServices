2f4346753d98e6e90e41a5e71611e8504f2a861f
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-06-13 02:26:33 -0400

    still coding geofence example, most of it is done
    not adding the fence or detecting it

--- geofencing/src/main/java/com/nullcognition/geofencing/GeofenceTransitionsIntentService.java
+++ geofencing/src/main/java/com/nullcognition/geofencing/GeofenceTransitionsIntentService.java
@@ -2,9 +2,11 @@ package com.nullcognition.geofencing;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -17,7 +19,7 @@ public class GeofenceTransitionsIntentService extends IntentService{
 	private static final String ACTION_ADD_GEOFENCE = "com.nullcognition.geofencing.action.ACTION_ADD_GEOFENCE";
 	private static final String GEOFENCING_EVENT = "com.nullcognition.geofencing.extra.GEOFENCING_EVENT ";
 
-	public static void startActionFoo(Context context ){ // , GeofencingEvent geofencingEvent
+	public static void startActionFoo(Context context, GeofencingEvent geofencingEvent){
 		Intent intent = new Intent(context, GeofenceTransitionsIntentService.class);
 		intent.setAction(ACTION_ADD_GEOFENCE);
 //		intent.putExtra(GEOFENCING_EVENT,geofencingEvent);
@@ -31,33 +33,32 @@ public class GeofenceTransitionsIntentService extends IntentService{
 	@Override
 	public void onCreate(){
 		super.onCreate();
-		sendNotification("notification sent");
 	}
+
 	@Override
 	protected void onHandleIntent(Intent intent){
-//		if(intent != null){
-//			final String action = intent.getAction();
+		if(intent != null){
+			final String action = intent.getAction();
 //			if(ACTION_ADD_GEOFENCE.equals(action)){
-//				final GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
-//				if(geofencingEvent.hasError()){
-//					Log.e("logErr", "error geofencing event:" + geofencingEvent.getErrorCode());
-//				}
-//				handleActionFoo(geofencingEvent);
+				final GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+				if(geofencingEvent.hasError()){
+					Log.e("logErr", "error geofencing event:" + geofencingEvent.getErrorCode());
+				}
+				handleActionFoo(geofencingEvent);
 //			}
-//		}
+		}
 	}
 
 	private void handleActionFoo(GeofencingEvent geofencingEvent){
 		int geofenceTransition = geofencingEvent.getGeofenceTransition();
 
-		List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
-		String geofenceTransitionDetails = null;
-		for(Geofence g : geofences){ geofenceTransitionDetails += g.getRequestId() + "\n"; }
-		// TextUtils.join("delimiter", geofences);
-		sendNotification(geofenceTransitionDetails);
-
 		switch(geofenceTransition){
 			case Geofence.GEOFENCE_TRANSITION_ENTER:
+				List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
+				String geofenceTransitionDetails = null;
+				for(Geofence g : geofences){ geofenceTransitionDetails += g.getRequestId() + "\n"; }
+				// TextUtils.join("delimiter", geofences);
+				sendNotification(geofenceTransitionDetails);
 				break;
 			case Geofence.GEOFENCE_TRANSITION_EXIT:
 				break;
@@ -66,20 +67,28 @@ public class GeofenceTransitionsIntentService extends IntentService{
 			default:
 				throw new RuntimeException("geofence transition input invalid:" + geofenceTransition);
 		}
-
-
 	}
+
 	public void sendNotification(final String geofenceTransitionDetails){
 
-		NotificationCompat.Builder mBuilder =
+		Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+		android.support.v4.app.TaskStackBuilder stackBuilder = android.support.v4.app.TaskStackBuilder.create(this);
+		stackBuilder.addParentStack(MainActivity.class);
+		stackBuilder.addNextIntent(notificationIntent);
+		PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		NotificationCompat.Builder notificationBuilder =
 				new NotificationCompat.Builder(this)
+						.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.common_full_open_on_phone))
 						.setSmallIcon(R.drawable.ic_plusone_medium_off_client)
+						.setColor(Color.BLUE)
 						.setContentTitle("My notification")
-						.setContentText(geofenceTransitionDetails);
+						.setContentText(geofenceTransitionDetails)
+						.setContentIntent(pendingIntent);
+		notificationBuilder.setAutoCancel(true);
 
 		int mNotificationId = 001;
-		NotificationManager mNotifyMgr =
-				(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		mNotifyMgr.notify(mNotificationId, mBuilder.build());
+		NotificationManager mNotifyMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotifyMgr.notify(mNotificationId, notificationBuilder.build());
 	}
 }
--- geofencing/src/main/java/com/nullcognition/geofencing/MainActivity.java
+++ geofencing/src/main/java/com/nullcognition/geofencing/MainActivity.java
@@ -1,8 +1,11 @@
 package com.nullcognition.geofencing;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +15,14 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
                                                       GoogleApiClient.OnConnectionFailedListener,
@@ -21,37 +30,46 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
                                                       LocationListener{
 
 	protected GoogleApiClient googleApiClient;
-
+	protected ArrayList<Geofence> geofenceArrayList;
 
 	@Override
 	public void onLocationChanged(final Location location){
 
 	}
+
 	@Override
 	public void onResult(final Status status){
-
+		if(status.isSuccess()){Toast.makeText(this, "Geofences Added", Toast.LENGTH_SHORT).show();}
+		else{Log.e("logErr", status.getStatusMessage()); }
 	}
+
 	@Override
 	public void onConnected(final Bundle bundle){
 
 	}
 	@Override
 	public void onConnectionSuspended(final int i){
+		googleApiClient.connect();
 
 	}
 	@Override
 	public void onConnectionFailed(final ConnectionResult connectionResult){
-
+		Log.e("logErr", "onConnection failed:" + connectionResult.getErrorCode());
 	}
+
 	@Override
 	protected void onStart(){
 		super.onStart();
-		googleApiClient.connect();
+		if(!googleApiClient.isConnecting() || !googleApiClient.isConnected()){
+			googleApiClient.connect();
+		}
 	}
 
 	@Override
 	protected void onStop(){
-		if(googleApiClient.isConnected()){googleApiClient.disconnect();}
+		if(googleApiClient.isConnecting() || googleApiClient.isConnected()){
+			googleApiClient.disconnect();
+		}
 		super.onStop();
 	}
 
@@ -67,14 +85,54 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 					.addOnConnectionFailedListener(this)
 					.build();
 		}
+		geofenceArrayList = new ArrayList<Geofence>();
+		populateGeofenceList();
+	}
+	private void populateGeofenceList(){
+		for(Map.Entry<String, LatLng> entry : Constants.BAY_AREA_LANDMARKS.entrySet()){
+
+			geofenceArrayList.add(new Geofence.Builder()
+					.setRequestId(entry.getKey())
+					.setCircularRegion(entry.getValue().latitude, entry.getValue().longitude, Constants.GEOFENCE_RADIUS_IN_METERS)
+					.setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+					.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+					.build());
+		}
 	}
 
 	public void addGeofence(final View view){
+		if(!googleApiClient.isConnected()){
+			Toast.makeText(this, getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
+			return;
+		}
 
-		Toast.makeText(MainActivity.this, String.valueOf(googleApiClient.isConnected()), Toast.LENGTH_SHORT).show();
-		GeofenceTransitionsIntentService.startActionFoo(this);
-		// do button logic here
+		// A pending intent that that is reused when calling removeGeofences(). This
+		// pending intent is used to generate an intent when a matched geofence
+		// transition is observed.
+		try{
+			LocationServices.GeofencingApi.addGeofences(
+					googleApiClient, getGeofencingRequest(), getGeofencePendingIntent()
+			).setResultCallback(this); // Result processed in onResult().
+		}
+		catch(SecurityException securityException){
+			// Catch exception generated if the app does not use ACCESS_FINE_LOCATION permission.
+		}
 	}
+
+	private PendingIntent getGeofencePendingIntent(){
+		Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+		// We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling addgeoFences()
+		return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	}
+
+
+	private GeofencingRequest getGeofencingRequest(){
+		GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+		builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+		builder.addGeofences(geofenceArrayList);
+		return builder.build();
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu){
