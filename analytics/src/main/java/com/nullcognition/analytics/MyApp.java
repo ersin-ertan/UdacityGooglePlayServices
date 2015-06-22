1d8fe5d2c7f6b87bc0f555196ee077508f2cfd7a
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-06-22 13:25:42 -0400

    put the analytics id in the xml folder in the root of res

---
+++ analytics/src/main/java/com/nullcognition/analytics/MyApp.java
@@ -0,0 +1,25 @@
+package com.nullcognition.analytics;// Created by ersin on 22/06/15
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+public class MyApp extends Application{
+
+	Tracker tracker;
+
+	public void startTracking(){
+		if(tracker == null){
+			GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+			tracker = ga.newTracker(R.xml.analytics_id);
+			ga.enableAutoActivityReports(this);
+		}
+	}
+
+	public Tracker getTracker(){
+		startTracking();
+		return tracker;
+	}
+
+}
